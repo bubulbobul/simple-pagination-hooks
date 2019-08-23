3ac1ea724586be5091339a03b922e224b47704e5
bubulbobul <windam@live.fr>
2019-08-23 08:44:16 +0530

    Updates

---
+++ precache-manifest.d4995bac6d71cb22014c50d6c58a7fc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b335b225a206ed7d001731b3ae063c37",
+    "url": "/simple-pagination-hooks/index.html"
+  },
+  {
+    "revision": "195366b986fa04c3e04c",
+    "url": "/simple-pagination-hooks/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b8e401a3df812c19021d",
+    "url": "/simple-pagination-hooks/static/js/2.700c56b0.chunk.js"
+  },
+  {
+    "revision": "195366b986fa04c3e04c",
+    "url": "/simple-pagination-hooks/static/js/main.94f5887f.chunk.js"
+  },
+  {
+    "revision": "36676419167d4af81a37",
+    "url": "/simple-pagination-hooks/static/js/runtime~main.599cbc49.js"
+  }
+]);
