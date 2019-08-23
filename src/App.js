0188315ef9434f01ef3b0ce8e9b4bcdeb0d9576c
bubulbobul <windam@live.fr>
2019-08-23 08:40:35 +0530

    add onclick to change page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage] = useState(7);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -29,11 +29,14 @@ function App() {
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">Simple Pagination with react hooks</h1>
       <Posts posts={currentPosts} loading={loading} />
-      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate} />
     </div>
   );
 }
