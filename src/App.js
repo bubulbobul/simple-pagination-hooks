7aad349d45384072972552aa77b430462cadc8ca
bubulbobul <windam@live.fr>
2019-08-23 08:25:44 +0530

    gettins the posts and display it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import Posts from "./components/Posts"
+
+const API_URL = "https://jsonplaceholder.typicode.com/posts"
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPages, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get(API_URL);
+      setPosts(res.data);
+      setLoading(false)
+    }
+
+    fetchPosts();
+  }, []);
+
+  console.log(posts)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">Simple Pagination with react hooks</h1>
+      <Posts posts={posts} loading={loading} />
     </div>
   );
 }
